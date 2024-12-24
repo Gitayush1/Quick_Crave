11fa53a659ca1662e45d65e3da4365fdb34ab528
Gitayush1 <kunwarayush23@gmail.com>
2024-12-24 17:41:35 +0530

    commit 2nd time

--- Inception/app2.js
+++ Inception/app2.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 /* 
 
 <div id="parent">
@@ -14,6 +16,7 @@
 ReactElement(Object) => HTML(Browser Understands)
 
 */
+
 
 const parent = React.createElement('div',{id : 'parent'},[
     React.createElement('div',{id : 'child'},[

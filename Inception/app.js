c35107b61f4fecb60964adc6ef2b77512efd728b
Gitayush1 <kunwarayush23@gmail.com>
2024-12-24 13:12:08 +0530

    Initial commit

---
+++ Inception/app.js
@@ -0,0 +1,5 @@
+const heading = document.createElement("h1");
+heading.innerHTML = "Hello world from JS";
+
+const root = document.getElementById("root1");
+root.appendChild(heading);
---
+++ Inception/app2.js
@@ -0,0 +1,31 @@
+/* 
+
+<div id="parent">
+  <div id=child>
+    <h1>I'm h1 Tag</h1>
+    <h2>I'm h2 Tag</h2>
+  </div>
+  <div id=child2>
+    <h1>I'm h1 Tag</h1>
+    <h2>I'm h2 Tag</h2>
+  </div>
+</div>
+
+ReactElement(Object) => HTML(Browser Understands)
+
+*/
+
+const parent = React.createElement('div',{id : 'parent'},[
+    React.createElement('div',{id : 'child'},[
+        React.createElement('h1',{},"I'm H1 Tag"),
+        React.createElement('h2',{},"I'm H2 Tag")
+    ])
+], React.createElement('div',{id : 'child2'},[
+        React.createElement('h1',{},"I'm H1 Tag"),
+        React.createElement('h2',{},"I'm H2 Tag")
+])
+);
+const root2 = ReactDOM.createRoot(document.getElementById('root2'))
+root2.render(parent)
+
+console.log(parent);
